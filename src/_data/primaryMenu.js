dc14eec2f9757b8df18582c408d5ef418fc1a25b
Richard Herbert <richard@infoweb.co.uk>
2020-11-25 16:40:20 +0000

    Added primary menu data

---
+++ src/_data/primaryMenu.js
@@ -0,0 +1,6 @@
+module.exports = [
+	{ text: 'Home', path: '/' }
+	,{ text: 'Footpaths', path: '/footpaths/' }
+	,{ text: 'About us', path: '/about/' }
+	,{ text: 'Contact us', path: '/contact/' }
+]
