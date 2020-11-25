6a4b041d502c2a8930af813f90e5083a97e992cb
Richard Herbert <richard@infoweb.co.uk>
2020-11-25 11:21:33 +0000

    Move content to src folder

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,8 @@ module.exports = function( eleventyConfig ) {
 	eleventyConfig.setQuietMode( true );
 	eleventyConfig.setTemplateFormats( 'html,md,liquid' );
 
-	eleventyConfig.addPassthroughCopy( './assets/' );
-	eleventyConfig.addPassthroughCopy( './robots.txt' );
+	eleventyConfig.addPassthroughCopy( './src/assets/' );
+	eleventyConfig.addPassthroughCopy( './src/robots.txt' );
 
 	eleventyConfig.addFilter( 'dump', function( anything ) {
 		console.log( 'dump:', anything );
