81148327edcd540fb3cfb35dd3c710c9f4705aa9
Richard Herbert <richard@infoweb.co.uk>
2020-12-01 18:12:29 +0000

    Added 404 support

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const fs = require( 'fs' );
+
 module.exports = function( eleventyConfig ) {
 	eleventyConfig.setQuietMode( true );
 	eleventyConfig.setTemplateFormats( 'html,md,liquid' );
@@ -13,5 +15,18 @@ module.exports = function( eleventyConfig ) {
 		ui: false
 		,ghostMode: false
 		,logLevel: 'silent'
+		,callbacks: {
+			ready: function ( err, bs ) {
+				bs.addMiddleware( "*", ( req, res ) => {
+					const content_404 = fs.readFileSync( '_site/404.html' );
+					// Provides the 404 content without redirect.
+					res.write( content_404 );
+					// Add 404 http status code in request header.
+					// res.writeHead(404, { "Content-Type": "text/html" });
+					res.writeHead( 404 );
+					res.end();
+				} );
+			}
+		}
 	} );
 };
