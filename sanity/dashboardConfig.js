d531b68bfcf223f46011710f8c67f28caa1e9d6c
Richard Herbert <richard@infoweb.co.uk>
2020-12-10 10:29:48 +0000

    Enviroment variables corrected

--- sanity/dashboardConfig.js
+++ sanity/dashboardConfig.js
@@ -3,13 +3,13 @@ export default {
 		{
 			name: 'netlify'
 			,options: {
-				title: 'Deploy Content Changes to Web Site'
+				title: 'Deploy Content Changes to the Preview Site'
 				,sites: [
 					{
 						title: 'Accessible Duffield'
 						,name: 'accessibleduffield'
-						,apiId: process.env.SANITY_TOKEN
-						,buildHookId: process.env.NETLIFY_BUILD_HOOK
+						,apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID
+						,buildHookId: process.env.SANITY_STUDIO_NETLIFY_BUILD_HOOK_PREVIEW
 					},
 				]
 			}
