c23f621aa6801501da9b1de97d26613bdaaabc51
Richard Herbert <richard@infoweb.co.uk>
2021-01-05 14:40:06 +0000

    Use extended colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
+const colors = require( 'tailwindcss/colors' );
+
 module.exports = {
 	purge: [
 		'./**/*.html'
 	]
 	,theme: {
 		extend: {}
+		,colors: {
+			transparent: 'transparent'
+			,current: 'currentColor'
+			,gray: colors.trueGray
+			,white: colors.white
+			,lime: colors.lime
+		}
 	}
 	,variants: {}
 	,plugins: []
