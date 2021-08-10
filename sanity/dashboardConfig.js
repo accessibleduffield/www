c09fda727fa48b1ac4839a8c8694771137075adf
Richard Herbert <richard@infoweb.co.uk>
2021-08-10 13:27:05 +0100

    Added deploy for LIVE

--- sanity/dashboardConfig.js
+++ sanity/dashboardConfig.js
@@ -3,10 +3,16 @@ export default {
 		{
 			name: 'netlify'
 			,options: {
-				title: 'Deploy Content Changes to the Preview Site'
+				title: 'Deploy Content Changes to the LIVE or PREVIEW Site'
 				,sites: [
 					{
-						title: 'Accessible Duffield'
+						title: 'Accessible Duffield LIVE'
+						,name: 'accessibleduffield'
+						,apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID
+						,buildHookId: process.env.SANITY_STUDIO_NETLIFY_BUILD_HOOK
+					},
+					{
+						title: 'Accessible Duffield PREVIEW'
 						,name: 'accessibleduffield'
 						,apiId: process.env.SANITY_STUDIO_NETLIFY_API_ID
 						,buildHookId: process.env.SANITY_STUDIO_NETLIFY_BUILD_HOOK_PREVIEW
